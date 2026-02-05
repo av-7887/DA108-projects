707fa8c536509fde594c64ec10cbc752c6d2fa5c
Aryan Verma <257152630+av7887-student@users.noreply.github.com>
2026-02-05 19:00:25 +0530

    age calculator

--- age_calculator.py
+++ age_calculator.py
@@ -2,9 +2,8 @@ On branch main
 Your branch is up to date with 'origin/main'.
 
 Changes not staged for commit:
-  (use "git add/rm <file>..." to update what will be committed)
+  (use "git add <file>..." to update what will be committed)
   (use "git restore <file>..." to discard changes in working directory)
-	deleted:    README.txt
 	modified:   age_calculator.py
 
 Untracked files:
